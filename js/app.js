63e2f45cc15b81c3ff6fdad356d40a93a53abd05
dirtroaddev <rongoodwindev@gmail.com>
2024-02-05 20:26:38 -0500

    Fiished this.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
 const btn = document.getElementById('weather__btn');
 const impData = document.getElementById('weather_city');
+const dDisplay = document.querySelector('.data__display');
 
 
-
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', async (e) => {
     e.preventDefault();
-
+    dDisplay.innerHTML = '';
+    async function getWeather() {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=dc499f9dd5e341209ca23852240502&q=${impData.value}`);
+        const data = await response.json();
+    
+        return data;
+    
+    }
+    
+    let wData = await getWeather()
+    const h1 = document.createElement('h1');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    img.src = wData.current.condition.icon
+    p.innerText = wData.current.condition.text
+    h1.innerText = wData.location.name;
+    dDisplay.appendChild(h1);
+    dDisplay.appendChild(p);
+    dDisplay.appendChild(img);
+    console.log(wData);
 });
-async function getWeather() {
-    const response = await fetch('https://api.weatherapi.com/v1/current.json?key=dc499f9dd5e341209ca23852240502&q=32024');
-    const data = await response.json();
-
-    return data.current;
-
-}
 
 
-getWeather().then(response => console.log(response))
+
